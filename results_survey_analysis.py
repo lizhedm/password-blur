24d015f3bdb38156651ced716e02f050a53633d1
李喆 <lizhe@dhcp-10-176-48-157.dynamic.eduroam.mwn.de>
2017-07-27 13:08:43 +0200

    Update results_survey_analysis.py

--- results_survey_analysis.py
+++ results_survey_analysis.py
@@ -17,39 +17,63 @@ def levenshtein(s, t):
                 v0[j] = v1[j]
                 
         return v1[len(t)]
-    
+
 import csv
+import pandas as pd
 
-# set right answer list
-typB_right_list = []
-typC_right_list = []
-file = open('right_answers.txt','r')
-the_list = file.read().split('\n')
-for row in the_list:
-    right_answer_list = row.split(',')
-    typB_right_list.append(right_answer_list[0])
-    typC_right_list.append(right_answer_list[1])
+# get right answer list
+df = pd.read_csv('right_answers.csv', encoding = "ISO-8859-1")
+typB_right_list = df['TypB'].tolist()
+typC_right_list = df['TypC'].tolist()
+# split() works not well
+# file = open('right_answers.csv','r')
+# the_list = file.read().split('\n')
+# for row in the_list:
+#     right_answer_list = row.split(',')
+#     typB_right_list.append(right_answer_list[0])
+#     typC_right_list.append(right_answer_list[1])
+# typB_right_list = typB_right_list[1:]
+# typC_right_list = typC_right_list[1:]
 
-# get users' answers from csv file
-typB_list = []
-typC_list = []
-file = open('results-survey.csv', 'r', encoding = "ISO-8859-1")
-readCSV = file.read()
-row_list = readCSV.split('\n')
+# create a list of lists
 typB_distance_list = [[] for x in range(8)]
 typC_distance_list = [[] for x in range(8)]
+
+# get users' answers from csv file
+df = pd.read_csv('results-survey.csv', encoding = "ISO-8859-1")
 for i in range(8):
-    for item in row_list:
-        row_str_list = item.split(',')
-        # claculate levenshtein distance
-        # clean the empty string
-        if row_str_list[6*i + 9] != '':
-            typB_distance = levenshtein(row_str_list[6*i + 9],typB_right_list[i + 1])
+    for index, row in df.iterrows():
+        if pd.isnull(row[6*i + 9]):
+            row[6*i + 9] = ''
+        if row[6*i + 9] != '':
+            typB_distance = levenshtein(row[6*i + 9],typB_right_list[i])
             typB_distance_list[i].append(typB_distance)
-        if row_str_list[6*i + 10] != '':
-            typC_distance = levenshtein(row_str_list[6*i + 10],typC_right_list[i + 1])
+        
+        if pd.isnull(row[6*i + 10]):
+            row[6*i + 10] = ''
+        if row[6*i + 10] != '':
+            typC_distance = levenshtein(row[6*i + 10],typC_right_list[i])
             typC_distance_list[i].append(typC_distance)
-    print('distance of ' + typB_right_list[i + 1] + ':')
-    print(typB_distance_list[i][1:])
-    print('distance of ' + typC_right_list[i + 1] + ':')
-    print(typC_distance_list[i][1:])
+        
+    print('\ndistance of ' + typB_right_list[i] + ':\n', typB_distance_list[i][:], 
+          '\ndistance of ' + typC_right_list[i] + ':\n', typC_distance_list[i][:])
+
+    
+# split() works not well
+# for i in range(8):
+#     for item in row_list:
+#         row_str_list = item.split(',')
+# #         print(len(row_str_list))
+#         # claculate levenshtein distance
+#         # clean the empty string
+#         if row_str_list[6*i + 9] != '':
+#             typB_distance = levenshtein(row_str_list[6*i + 9],typB_right_list[i])
+#             typB_str = '(' + row_str_list[6*i + 9] + ',' + typB_right_list[i] + ')' + str(typB_distance)
+#             typB_distance_list[i].append(typB_str)
+#         if row_str_list[6*i + 10] != '':
+#             typC_distance = levenshtein(row_str_list[6*i + 10],typC_right_list[i])
+#             typC_distance_list[i].append(typC_distance)
+#     print('distance of ' + typB_right_list[i] + ':')
+#     print(typB_distance_list[i][1:])
+#     print('distance of ' + typC_right_list[i] + ':')
+#     print(typC_distance_list[i][1:])
