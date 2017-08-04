e425154b3690c40e1420987a2a8a8b4718b4a3db
李喆 <lizhe@dhcp-10-176-57-40.dynamic.eduroam.mwn.de>
2017-08-04 14:17:15 +0200

    Update

--- results_survey_analysis.py
+++ results_survey_analysis.py
@@ -41,23 +41,53 @@ typC_distance_list = [[] for x in range(8)]
 
 # get users' answers from csv file
 df = pd.read_csv('results-survey.csv', encoding = "ISO-8859-1")
+df2 = df.copy()
 for i in range(8):
     for index, row in df.iterrows():
         if pd.isnull(row[6*i + 9]):
             row[6*i + 9] = ''
-        if row[6*i + 9] != '':
-            typB_distance = levenshtein(row[6*i + 9],typB_right_list[i])
-            typB_distance_list[i].append(typB_distance)
+#         if row[6*i + 9] != '':
+        typB_distance = levenshtein(row[6*i + 9],typB_right_list[i])
+        if typB_distance > len(typB_right_list[i]):
+            typB_distance = len(typB_right_list[i])
+#         this does not works, do not know why
+#         if row[6*i + 9] != '':
+#             df2.set_value(index,6*i + 9,typB_distance)
+#             print(row[6*i + 9],df2.iloc[index][6*i + 9])
+        if row[6*i + 9] == '':
+            typB_distance = None
+        typB_distance_list[i].append(typB_distance)
         
         if pd.isnull(row[6*i + 10]):
             row[6*i + 10] = ''
-        if row[6*i + 10] != '':
-            typC_distance = levenshtein(row[6*i + 10],typC_right_list[i])
-            typC_distance_list[i].append(typC_distance)
+#         if row[6*i + 10] != '':
+        typC_distance = levenshtein(row[6*i + 10],typC_right_list[i])
+        if typC_distance > len(typC_right_list[i]):
+            typC_distance = len(typC_right_list[i])
+        if row[6*i + 10] == '':
+            typC_distance = None
+        typC_distance_list[i].append(typC_distance)
         
-    print('\ndistance of ' + typB_right_list[i] + ':\n', typB_distance_list[i][:], 
-          '\ndistance of ' + typC_right_list[i] + ':\n', typC_distance_list[i][:])
+df2.colorhalftone6TypB = typB_distance_list[0]
+df2.colorhalftone6TypC = typC_distance_list[0]
+df2.colorhalftone5TypB = typB_distance_list[1]
+df2.colorhalftone5TypC = typC_distance_list[1]
+df2.crystallize8TypB = typB_distance_list[2]
+df2.crystallize8TypC = typC_distance_list[2]
+df2.crystallize7TypB = typB_distance_list[3]
+df2.crystallize7TypC = typC_distance_list[3]
+df2.gauss8TypB = typB_distance_list[4]
+df2.gauss8TypC = typC_distance_list[4]
+df2.gauss75TypB = typB_distance_list[5]
+df2.gauss75TypC = typC_distance_list[5]
+df2.mosaik11TypB = typB_distance_list[6]
+df2.mosaik11TypC = typC_distance_list[6]
+df2.mosaik10TypB = typB_distance_list[7]
+df2.mosaik10TypC = typC_distance_list[7]
+#     print('\ndistance of ' + typB_right_list[i] + ':\n', typB_distance_list[i][:], 
+#           '\ndistance of ' + typC_right_list[i] + ':\n', typC_distance_list[i][:])
 
+df2.to_csv('results-survey-after-calculated.csv',sep=',', encoding='ISO-8859-1')
     
 # split() works not well
 # for i in range(8):
