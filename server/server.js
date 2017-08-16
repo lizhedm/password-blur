b81f917629c3d223b3937fe0efdfd1d7d8d2ecc3
李喆 <lizhe@Zhes-MacBook-Pro.local>
2017-08-17 01:54:23 +0200

    Using node.js to built server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const csv = require('csv-express')
+const path = require('path')
+const fs = require('fs')
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/', express.static(path.join(__dirname, '../html_prototype/')))
+app.post('/data', function(req, res) {
+  // console.log(req.body['typing_task_seconds'])
+  data_str = req.body['user_id'] + ',' + req.body['input_field'] + ',' + req.body['time'] + ',' + req.body['input_text'] + '\n'
+  fs.appendFileSync('file.csv', data_str);
+  // fs.writeFile('file.csv', data_str, function (err) {
+  //   console.log(req.body);
+  // });
+  res.json({'status': true})
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
