7ecca36b5ecf8fc2c8979270f07d6e2be5582bba
李喆 <lizhe@Zhes-MacBook-Pro.local>
2017-08-17 20:16:54 +0200

    Add cookie feature

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
+const cookieParser = require('cookie-parser');
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 const csv = require('csv-express')
 const path = require('path')
 const fs = require('fs')
+app.use(cookieParser());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({extended:false}))
 app.use('/', express.static(path.join(__dirname, '../html_prototype/')))
 app.post('/data', function(req, res) {
   // console.log(req.body['typing_task_seconds'])
-  data_str = req.body['user_id'] + ',' + req.body['input_field'] + ',' + req.body['time'] + ',' + req.body['input_text'] + '\n'
+  data_str = '"' + req.body['user_id'] + '"' + ',' + '"' + req.body['input_field'] + '"' + ',' + '"' + req.body['time'] + '"' + ',' + '"' + req.body['input_text'] + '"' + '\n'
   fs.appendFileSync('file.csv', data_str);
   // fs.writeFile('file.csv', data_str, function (err) {
   //   console.log(req.body);
